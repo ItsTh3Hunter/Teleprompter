f7f5b25929d18223f2f1c0c072a2d2db98727c4b
Predator <coding.carvalho@gmail.com>
2026-04-21 22:46:20 -0300

    Commit inicial do projeto

---
+++ Teleprompter/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Teleprompter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+            private void btnCorTexto_Click(object sender, RoutedEventArgs e)
+            {
+            }
+            private void btnCorFundo_Click(object sender, RoutedEventArgs e)
+            {
+            }
+            private void btnIniciar_Click(object sender, RoutedEventArgs e)
+            {
+            }
+    }
+    }
